61ac1a455cd20039f3c34827f32fa80c3588a3b0
Ananya Shukla <65492788+ananya123-gif@users.noreply.github.com>
2022-08-03 11:33:30 +0530

    Program 6

---
+++ PrepInsta_100_Code/6) Find the Greatest of the Three Numbers.py
@@ -0,0 +1,14 @@
+# Find the Greatest of the Three Numbers
+# Given three integer inputs the objective is to find the largest among them.
+# Therefore we write a code to Find the Greatest of the Three Numbers in Python.
+
+# Example
+# Input : 20 30 10
+# Output : 30
+
+def greatest_three_number(num1,num2,num3):
+    sol=max(num1,num2,num3)
+    return sol
+  
+num1,num2,num3=map(int,input().split(" "))
+print(greatest_three_number(num1,num2,num3))
