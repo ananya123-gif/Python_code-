a9b0840a3a2914afe31f526c7b459a4b26c0bf10
Ananya Shukla <65492788+ananya123-gif@users.noreply.github.com>
2022-08-03 11:00:04 +0530

    program 1

---
+++ PrepInsta_100_Code/1) Check if a Number is Positive and Negative in Python.py
@@ -0,0 +1,11 @@
+#Check if a Number is Positive and Negative in Python
+def positive_or_negative(num):
+    if(num>0):
+        print(num," is Positive")
+    elif(num<0):
+        print(num," is Negative")
+    else:
+        print("Zero")
+
+num=int(input())
+positive_or_negative(num)
