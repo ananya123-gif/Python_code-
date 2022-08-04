0c3f34bd0365ce9fc24e9daa65661ee9868042ea
Ananya Shukla <65492788+ananya123-gif@users.noreply.github.com>
2022-08-04 16:36:55 +0530

    Program 8

---
+++ PrepInsta_100_Code/8) Check Whether a Number is a Prime or Not.py
@@ -0,0 +1,22 @@
+# Check Whether a Number is a Prime or Not in Python
+# Given an integer input the objective is to write a program to Check Whether a Number is a Prime or Not in Python Language.
+
+# Example
+# Input : 11
+# Output : 11 is a Prime
+
+def prime(num):
+    if(num<=1):
+        print(num,"is Not Prime")
+    else:
+        c=0
+        for i in range(1,num+1):
+            if(num%i==0):
+                c+=1
+        if(c==2):
+            print(num,"is Prime")
+        else:
+            print(num,"is not Prime")
+
+num=int(input())
+prime(num)
