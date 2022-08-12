9e8adaca3bc2bc8c99ea1979a7f6af1ba901109f
Ananya Shukla <65492788+ananya123-gif@users.noreply.github.com>
2022-08-12 21:23:21 +0530

    Program 19

---
+++ PrepInsta_100_Code/Working with Numbers/19) Calculating the area of circle.py
@@ -0,0 +1,11 @@
+# Calculating the area of circle
+
+# The formula for evaluating the area of circle is
+# Area of circle using radius=   Pi*r*r    ( r is the radius of circle)
+
+import math
+def area_of_circle(r):
+    return math.pi*r*r
+
+r=int(input())
+print(area_of_circle(r))
