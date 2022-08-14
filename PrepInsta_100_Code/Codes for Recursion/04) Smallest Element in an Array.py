a960ae2501a32587e2711598e53e0d6b5e712c79
Ananya Shukla <65492788+ananya123-gif@users.noreply.github.com>
2022-08-14 21:19:33 +0530

    Program 04

---
+++ PrepInsta_100_Code/Codes for Recursion/04) Smallest Element in an Array.py
@@ -0,0 +1,16 @@
+# Smallest Element in an Array
+# Given an array of integers “A”, the task is to write a Program for Finding the Smallest Element
+# in an Array using recursion algorithm.
+
+# Input: A = [1, 4, 3, -5, -4, 8, 6]
+# Output: -5
+
+def largest_element(A,n):
+    if(n==1):
+        return A[0]
+    else:
+        return min(A[n-1],largest_element(A,n-1))
+
+A=list(map(int,input().split()))
+n=len(A)
+print(largest_element(A,n))
